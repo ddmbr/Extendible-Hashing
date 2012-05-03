dc4d00bda4352bff43ec2e03f92fe94ff05e36fa
ddmbr <iddmbr@gmail.com>
2012-05-03 17:23:33 +0800

    add some structs

--- src/ehdb_schema.h
+++ src/ehdb_schema.h
@@ -1,5 +1,10 @@
+/*
+ * Here is the structure to define a schema,
+ * which is represented as a link list.
+ */
 struct ehdb_schema
 {
-    int col_num;
-    struct ehdb_col* head;
+    int col_num;            // Length of the link list
+
+    struct ehdb_col* head;  // The beginning of the link list
 };
