50d54e2a6dd86d6a133d3e12bdf891cddb5e351f
ddmbr <iddmbr@gmail.com>
2012-05-10 05:34:31 +0800

    change the prototype of hash function!

--- src/ehdb_hash.h
+++ src/ehdb_hash.h
@@ -1,25 +1,27 @@
-/* The hash function, 
- * begin from LSB
+/* The main hash function
  */
 int
-ehdb_hash_l(int key);
+ehdb_hash_func(int key, int depth);
+
+/* The hash function, begin from LSB
+ */
+int
+ehdb_hash_l(int key, int depth);
 
 /* The hash function, begin from MSB
  */
 int
-ehdb_hash_h(int key);
+ehdb_hash_h(int key, int depth);
 
 /* check whether a page is overflowed
  */
-int
+short
 ehdb_is_overflow(struct page_t* page_ptr);
-
-/* generate a link list
- */
-struct page_t*
-ehdb_bucket_grow(int bucket_id);
 
 /* write a record into a bucket
  */
 void
 ehdb_write_record(struct page_t* page_ptr, struct record_t record*);
+
+void
+ehdb_double_index(struct page_t *page_ptr);
