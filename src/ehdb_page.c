2bcaba18f8dfdf2e6a94e8bfbc7fcb39e1291a32
ddmbr <iddmbr@gmail.com>
2012-05-10 22:12:23 +0800

    some implementations

--- src/ehdb_page.c
+++ src/ehdb_page.c
@@ -1,30 +1,46 @@
+#include "ehdb_page.h"
+#include "ehdb_init.h"
+
+#define Record_size (13 + 3*2)
+
 int
 ehdb_get_record_num(struct page_t* page_ptr)
 {
-    return *((size_t*)(page_ptr + 1 * sizeof(int)));
+    return ((int*)page_ptr->head)[1];
 }
 
 int
 ehdb_get_depth(struct page_t* page_ptr)
 {
-    return *((size_t*)(page_ptr));
+    return ((int*)page_ptr->head)[0];
 }
 
 int
 ehdb_get_next_bucket(struct page_t* page_ptr)
 {
-    return *((size_t*)(page_ptr + 2 * sizeof(int)));
+    return ((int*)page_ptr->head)[2];
 }
 
 void*
 ehdb_free_begin(struct page_t* page_ptr)
 {
-    return 
+    return page_ptr + 4*sizeof(int) + ehdb_get_record_num(page_ptr) * Record_size;
+}
+
+int
+ehdb_get_record_string_len(page_t* page){
+    return ((int*)page_ptr->head)[3];
 }
 
 void*
 ehdb_free_end(struct page_t* page_ptr)
 {
-    size_t free_end = *((size_t*)(page_ptr + 3 * sizeof(int)));
-    return page_ptr + free_end;
+    int string_len = ehdb_get_record_string_len(page_ptr);
+    return page_ptr->head + (Page_size - string_len);
+}
+
+void
+ehdb_set_page_link(struct page_t* page_ptr, int page_id)
+{
+    ((int*)page_ptr->head)[3] = page_id;
 }
