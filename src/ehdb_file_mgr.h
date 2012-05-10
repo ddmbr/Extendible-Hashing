ceebe50410bf43485ad23ec15ca940c81a180ef3
ddmbr <iddmbr@gmail.com>
2012-05-11 00:19:17 +0800

    filemgr can be compiled

--- src/ehdb_file_mgr.h
+++ src/ehdb_file_mgr.h
@@ -1,17 +1,24 @@
 #pragma once
 #include "ehdb_page.h"
-/* This will generate a new page
- * the provided page_t will be modified.
+
+/* Initialize this module
  */
 void
-ehdb_new_page(page_ptr_t type, int depth);
+ehdb_file_init();
+
+/* This will generate a new page
+ * the provided page_t will be modified.
+ * return value is the bucket id
+ */
+int
+ehdb_new_page(page_type_t type, int depth);
 
 /* Providing page_t, page_id and page_type,
  * this method will copy the corresponding
  * page from disk to the memory
  */
 void
-ehdb_copy_from_file(page_t *page_ptr, int page_id, int page_type);
+ehdb_copy_from_file(struct page_t *page_ptr);
 
 /* this method will save the corresponding
  * page from memory to disk
