73f6a85ddefc47395eae8885940cd81dce29eff0
matrix <matrix0415@gmail.com>
2012-05-11 08:29:46 -0700

    50,82,83: error: excepected primary-expressions before ']' token

--- src/ehdb_query.c
+++ src/ehdb_query.c
@@ -18,7 +18,7 @@ void ehdb_query(int key, FILE *fout){
 
 	while(offset =ehdb_page_record2record(hash_bucket, offset, &record_data) !=-1){
 		record_array[while_count] =record_data;
-		quick_sorting(record_array , 0,record_length-1, record_length);
+		quick_sorting(record_array[], 0,record_length-1, record_length);
 		printf("%d|%d|%d|%d|%f|%f|%f|%f|%c|%c|%d|%d|%d|%c|%c|%c|\n", record_data[while_count].orderkey, record_data[while_count].partykey, record_data[while_count].suppkey, record_data[while_count].linenumber, record_data[while_count].quantity, record_data[while_count].extendedprice, record_data[while_count].discount, record_data[while_count].tax, record_data[while_count].returnflag, record_data[while_count].linestatus, record_data[while_count].shipdate, record_data[while_count].commitdate, record_data[while_count].receiptdate, record_data[while_count].shipinstruct, record_data[while_count].shipmode, record_data[while_count].comment);
 		);
 		fprintf(fout, "%d|%d|%d|%d|%f|%f|%f|%f|%c|%c|%d|%d|%d|%c|%c|%c|-1\n", record_data[while_count].orderkey, record_data[while_count].partykey, record_data[while_count].suppkey, record_data[while_count].linenumber, record_data[while_count].quantity, record_data[while_count].extendedprice, record_data[while_count].discount, record_data[while_count].tax, record_data[while_count].returnflag, record_data[while_count].linestatus, record_data[while_count].shipdate, record_data[while_count].commitdate, record_data[while_count].receiptdate, record_data[while_count].shipinstruct, record_data[while_count].shipmode, record_data[while_count].comment);
