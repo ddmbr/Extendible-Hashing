69318f3a6ecd939f3e03dcc574d814ac2bd2355c
ddmbr <iddmbr@gmail.com>
2012-05-14 12:10:57 +0800

    xxx

--- src/ehdb_hash.c
+++ src/ehdb_hash.c
@@ -3,7 +3,7 @@
 #include "ehdb_utils.h"
 #include <stdlib.h>
 #include <stdio.h>
-#define MAX_DEPTH 19
+#define MAX_DEPTH 30
 
 int
 ehdb_hash_func(int key, int depth)
@@ -19,11 +19,25 @@ ehdb_hash_func(int key, int depth)
 int
 ehdb_hash_h(int key, int depth)
 {
-    //TODO
-    int invert_key = 0, i;
-    for(i = 1; i <= MAX_DEPTH; i++)
-        invert_key += ((key & (1 << (i - 1))) > 0) << (MAX_DEPTH - i);
-    return ehdb_hash_l(invert_key, depth);
+    int invert = 0, i;
+
+    for(i = MAX_DEPTH - 1; i >= 0; i--)
+        if((key & (1 << i)) > 0)
+            break;
+    int sig = i;
+
+    //longer
+    if(sig >= depth)
+        key >>= (sig - depth);
+    //shorter
+    else
+        key <<= (depth - sig);
+
+    key &= ((1 << depth) - 1);
+
+    for(i = 1; i <= depth; i++)
+        invert += ((key & (1 << (i - 1))) > 0) << (depth - i);
+    return invert;
 }
 
 int
