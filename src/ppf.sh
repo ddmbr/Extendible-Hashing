86272108450790349d8add80f824d34c3a87fae0
Ray <ray040123@gmail.com>
2012-05-14 12:01:27 +0800

    update

--- src/ppf.sh
+++ src/ppf.sh
@@ -1,3 +1,3 @@
 perf record -g ./ehdb
-perf script | ./gprof2dot.py -f perf | xdot
-# perf script | ./gprof2dot.py -f perf | dot -Tpng -o profpic/proff_big_record_perf.png
+# perf script | ./gprof2dot.py -f perf | xdot
+perf script | ./gprof2dot.py -f perf | dot -Tpng -o profpic/proff_big_record_perf.png
