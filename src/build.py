837741226b8874b37928f4918845ccf48a196b60
ddmbr <iddmbr@gmail.com>
2012-05-18 03:41:37 +0800

    add README file. add new build script.

---
+++ src/build.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+tmpfile = '.mftemp'
+makecmd='make -B -f %s && mv -v ehdb ../bin/' % tmpfile
+
+import sys,os
+import re
+if len(sys.argv) != 3:
+    print """usage: build.py (H|L) (8|128)"""
+    exit(1)
+LH = sys.argv[1]
+PageNum = sys.argv[2]
+
+ehdbs = {
+    ('L', '8'):'ehdb_least_8',
+    ('H', '8'):'ehdb_most_8',
+    ('L', '128'):'ehdb_least_128',
+    ('H', '128'):'ehdb_most_128'
+    }
+
+if LH in 'LH' and PageNum in ('8', '128'):
+    s = open('Makefile', 'r').read()
+    s = re.sub('HashFunc=[LH]_HASH', 'HashFunc=%s_HASH' % LH, s)
+    open(tmpfile, 'w').write(s)
+    os.system(makecmd + ehdbs[LH, PageNum])
+    
--- src/ehdb_init.h
+++ src/ehdb_init.h
@@ -1,8 +1,6 @@
 #pragma once
 /* Initailize the system */
 #define PAGE_SIZE (8 * 1024)
-//#define PAGE_SIZE (1024)
-#define PAGE_NUM (8)
 
 extern int Global_depth;
 extern int Bucket_page_num;
